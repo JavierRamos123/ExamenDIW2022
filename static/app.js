8cad944d0d05d4eab6b7e7acf4dc3f013d96366d
EQUIPO1_ENTORNOS_DAW <javier.s270688@cesurformacion.com>
2022-12-19 21:33:41 +0100

    cambios finales

--- static/app.js
+++ static/app.js
@@ -2,6 +2,16 @@
 //JAVIER RAMOS DEL RIO
 // Completar JS aqui
 
-function boton1(id,visibility){
-    document.getElementById(id).style.display = visibility;
+function addClient(){
+    document.getElementById("container").style.display="none"
+    document.getElementById("cliente").style.display="block"
+}
+
+function volverAtras(){
+    document.getElementById("cliente").style.display="none"
+    document.getElementById("container").style.display="block"
+}
+
+window.onload = function hide(){
+    document.getElementById("cliente").style.display="none"
 }
